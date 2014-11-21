a8ed238a3292ba4d96eede0fdedbcd9eb1b14ddc
Simon Yoo <findsimon@gmail.com>
2014-11-21 02:02:46 +0000

    Change home path

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  root 'static_pages/home'
 
-  get 'static_pages/help'
+  match 'help', to: 'static_pages#help', via: 'get'
 
-  root 'static_pages#home'
 
-  match '/help', to: 'static_pages#help', via: 'get'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
